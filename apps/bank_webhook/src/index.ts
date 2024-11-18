2b510b0a4cc814fd66c7fa95d429cff4ea2e95ce
msabhii <msabhithakur7777@gmail.com>
2024-11-18 06:58:54 +0530

    Fixed react transaction

--- apps/bank_webhook/src/index.ts
+++ apps/bank_webhook/src/index.ts
@@ -1,52 +1,54 @@
-import db from "@repo/db/client";
 import express from "express";
-import z, { string } from "zod";
-
+import db from "@repo/db/client";
 const app = express();
-app.use(express.json());
 
-const userInputValidation = z.object({
-  token: string(),
-  userId: string(),
-  amount: string().optional(),
-});
+app.use(express.json());
 
 app.post("/hdfcWebhook", async (req, res) => {
   //TODO: Add zod validation here?
-  const paymentInformation = {
+  //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
+  const paymentInformation: {
+    token: string;
+    userId: string;
+    amount: string;
+  } = {
     token: req.body.token,
     userId: req.body.user_identifier,
     amount: req.body.amount,
   };
 
-  const validateInput = userInputValidation.safeParse(paymentInformation);
-  if (!validateInput) {
-    return res.status(400).json({ msg: "Not valid inputs" });
+  try {
+    await db.$transaction([
+      db.balance.updateMany({
+        where: {
+          userId: Number(paymentInformation.userId),
+        },
+        data: {
+          amount: {
+            // You can also get this from your DB
+            increment: Number(paymentInformation.amount),
+          },
+        },
+      }),
+      db.onRampTransaction.updateMany({
+        where: {
+          token: paymentInformation.token,
+        },
+        data: {
+          status: "Success",
+        },
+      }),
+    ]);
+
+    res.json({
+      message: "Captured",
+    });
+  } catch (e) {
+    console.error(e);
+    res.status(411).json({
+      message: "Error while processing webhook",
+    });
   }
-
-  await db.balance.update({
-    where: {
-      userId: paymentInformation.userId,
-    },
-    data: {
-      amount: {
-        increment: paymentInformation.amount,
-      },
-    },
-  });
-
-  await db.onRampTransaction.update({
-    where: {
-      token: paymentInformation.token,
-    },
-    data: {
-      status: "Success",
-    },
-  });
-
-  res.status(200).json({
-    msg: "captured",
-  });
-
-  // Update balance in db, add txn
 });
+
+app.listen(3003);
